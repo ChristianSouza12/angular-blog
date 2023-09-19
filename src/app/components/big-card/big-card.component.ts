86011cac514887627c9c670abf057ac8e6e2951d
ChristianSouza12 <christianlmsouza@outlook.com>
2023-09-19 08:34:55 -0300

    atualização dos components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -12,8 +12,10 @@ export class BigCardComponent implements OnInit {
   cardTitle:string = ""
   @Input()
   cardDescription:string = ``
+  @Input()
+  Id:string = "0"
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
---
+++ src/app/components/data/dataFake.ts
@@ -0,0 +1,49 @@
+export const dataFake = [
+  {
+    "id":"1",
+
+    "title": "Novos Títulos de Episódios do Anime Pokémon Horizontes.",
+    "description": "Novos títulos de episódios do anime Pokémon Horizontes foram revelados. Confira a programação para os próximos episódios: Título Original: Clash! The Galar Mine! Título Traduzido: Confronto! A Mina de Galar! Data de Exibição: 15/09/23",
+    "photoCover" : "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhBLAyRbzY_rtnoBUZLzc85WU9KWAbbt_QwJesTvkX6Z2TWrogLv400Y4IpHqbk0eK2L8h2G-s-e1OrJGZqLeDWjStSBIZhqZS9Gi-5loiqYA3eyI6cO_MIoQfL1HrxT28-UojQPHJdC4hiY5UrPaq4rc56ZTEOzi4dAZIO3RKVA4wXlGK33L-2XpRqx50/s800/pokemon-horizontes.png"
+  },
+  {
+    "id":"2",
+
+    "title": "O evento Espetáculo Psíquico voltou!",
+    "description": ` O evento Espetáculo Psíquico está de volta ao Pokémon GO cheio de novidades, incluindo a estreia de Mega Alakazam em Megarreides e o retorno de Deoxys!
+    Confira todos os detalhes do evento que começou hoje:
+
+    Data e hora
+    De terça-feira, 6 de setembro, às 10h, a segunda-feira, 12 de setembro de 2022, às 20h, horário local.
+
+    Estreia de Pokémon
+    Mega Alakazam fará sua estreia em Megarreides no Pokémon GO! Se o jogador tiver sorte, será possível encontrar um Alakazam Brilhante no momento da captura!
+    Os jogadores poderão aproveitar a aparição de Mega Alakazam para evoluir Kadabra (a evolução de Abra) e conseguir um Alakazam que sabe o ataque carregado Psíquico. Os Alakazam capturados em Megarreides neste período também saberão este movimento exclusivo.
+`
+
+
+    ,
+    "photoCover" : "https://lh3.googleusercontent.com/200vIpfQ5d7VO-olZ_MBrEtAH7aykE4wXl0rNEQJfxnlVSnaIkJolZ_E69RdG7qzO31Kf3g7BWZUXZ1HXE8Tf2OZYFX1bDSuX1D_VegDvPE5=w1440-e365"
+  },
+  {
+    "id":"3",
+
+    "title": "Celebrem Oddish com um Dia de Pesquisa!",
+    "description": "Treinadoras e Treinadores,Atenção ao ruído das folhas. Temos o prazer de anunciar um novo Dia de Pesquisa com Oddish!Coletem as tarefas de pesquisa de campo e vejam quantos Oddish vocês conseguirão encontrar!",
+    "photoCover" : "https://lh3.googleusercontent.com/i4jAa1kkTDgyH9M59WOf0pA-ex874C-XH8wGvUDefX0pZWf4JczHQ9tlRug-9z-bN_y3xcxnz4-c7GdqR2xA0PKunXDQZckiukzWg4qOa1tsnw=w1440-e365"
+  },
+  {
+    "id":"4",
+
+    "title": "Atualização da temporada da Liga de Batalha GO: Aventuras de Montão",
+    "description": `Data de início, redefinições de classificação e recompensas de final de Temporada da Liga de Batalha GO: Aventuras de Montão
+    A Liga de Batalha GO: Aventuras de Montão começará na sexta-feira, 1º de setembro de 2023, às 13h PDT (17h, horário de Brasília).
+
+    No início da temporada, ocorrerá o seguinte:
+
+    As recompensas de final de temporada estarão disponíveis na tela de batalha.
+    A classificação da Liga de Batalha GO será redefinida.
+    Os requisitos para subir na classificação serão os mesmos da temporada anterior.`,
+    "photoCover" : "https://lh3.googleusercontent.com/Usa4xRE3xG2_1Yt_FTmlL7X3qvnWwIh8Jn1KyGrN9Ak3K1vN6DrIGqTG6hmJGR6PNi4YdyLkpbHK1-sW4flMnLFWpcQOjGLi6mY9F7TnHjMlrDQ=w1440-e365"
+  },
+]
